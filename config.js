bfb1804979ca77d379d4e1517920a727bffc4410
Inzaaa347 <138759549+Inzaaa347@users.noreply.github.com>
2024-10-17 11:08:40 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=bvwCVJ5Y#AZihM8iaDReoX_ME7ociEnV06kEQLb-hJFovSmQT840",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
+MONGODB: process.env.MONGODB || "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority",//enter mongo db url
 };
